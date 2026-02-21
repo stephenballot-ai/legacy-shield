a5cfe210cb9a3b73752a73a7fb8f871833a0b4b3
Stephen Ballot <stephenballot@stephens-macbook-pro.home>
2026-02-21 10:12:33 +0100

    i18n: add Dutch (nl) locale and translations

--- packages/web/src/i18n/routing.ts
+++ packages/web/src/i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from 'next-intl/routing';
 import { createNavigation } from 'next-intl/navigation';
 
 export const routing = defineRouting({
-  locales: ['en', 'de', 'fr', 'it', 'es'],
+  locales: ['en', 'de', 'fr', 'it', 'es', 'nl'],
   defaultLocale: 'en',
   localePrefix: 'as-needed'
 });
